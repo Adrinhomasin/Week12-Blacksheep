764bbfbc90d090a6e49962a9a293867d5c809e9c
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:17:14 +0200

    blacksheep

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\Samples\autumn.txt";
+            string otherFilePath = @"C:\Users\opilane\Samples\winter.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string[] dataFromFile1 = File.ReadAllLines(otherFilePath);
+
+            File.WriteAllLines(dataFromFile, filePath);
+            File.WriteAllLines(otherFilePath, dataFromFile);
+        }
+    }
+}
---
+++ RandomQuote/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace RandomQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\users\opilane\samples\Randomquote.txt";            
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            Console.WriteLine(dataFromFile[0]);
+            Random rnd = new Random();
+            int randomindex = rnd.Next(dataFromFile.Length);
+            Console.WriteLine(dataFromFile[randomindex]);
+        }
+    }
+}
